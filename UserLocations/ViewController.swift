ac44d09288f9366852b094c57b5dfc653a7f47c2
Henry Savit <henrysavit@server.itshenry.com>
2015-12-08 20:23:38 -0500

    g

--- UserLocations/ViewController.swift
+++ UserLocations/ViewController.swift
@@ -12,6 +12,35 @@ import CoreLocation
 import MapKit
 import Foundation
 
+/*
+Methods
+
+on(event: String, callback: NormalCallback) -> NSUUID - Adds a handler for an event. Items are passed by an array. ack can be used to send an ack when one is requested. See example. Returns a unique id for the handler.
+once(event: String, callback: NormalCallback) -> NSUUID - Adds a handler that will only be executed once. Returns a unique id for the handler.
+onAny(callback:((event: String, items: AnyObject?)) -> Void) - Adds a handler for all events. It will be called on any received event.
+emit(event: String, _ items: AnyObject...) - Sends a message. Can send multiple items.
+emit(event: String, withItems items: [AnyObject]) - emit for Objective-C
+emitWithAck(event: String, _ items: AnyObject...) -> (timeoutAfter: UInt64, callback: (NSArray?) -> Void) -> Void - Sends a message that requests an acknowledgement from the server. Returns a function which you can use to add a handler. See example. Note: The message is not sent until you call the returned function.
+emitWithAck(event: String, withItems items: [AnyObject]) -> (UInt64, (NSArray?) -> Void) -> Void - emitWithAck for Objective-C. Note: The message is not sent until you call the returned function.
+connect() - Establishes a connection to the server. A "connect" event is fired upon successful connection.
+connect(timeoutAfter timeoutAfter: Int, withTimeoutHandler handler: (() -> Void)?) - Connect to the server. If it isn't connected after timeoutAfter seconds, the handler is called.
+close() - Closes the socket. Once a socket is closed it should not be reopened.
+reconnect() - Causes the client to reconnect to the server.
+joinNamespace() - Causes the client to join nsp. Shouldn't need to be called unless you change nsp manually.
+leaveNamespace() - Causes the client to leave the nsp and go back to /
+off(event: String) - Removes all event handlers for event.
+off(id id: NSUUID) - Removes the event that corresponds to id.
+removeAllHandlers() - Removes all handlers.
+Client Events
+
+connect - Emitted when on a successful connection.
+disconnect - Emitted when the connection is closed.
+error - Emitted on an error.
+reconnect - Emitted when the connection is starting to reconnect.
+reconnectAttempt - Emitted when attempting to reconnect.
+
+*/
+
 
 class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
 
